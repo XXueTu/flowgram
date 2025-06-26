ee7450e36dfdd49a9f9332a70189f183463078ff
xuetu.qiang <1227694865@qq.com>
2025-06-26 18:51:21 +0800

    feat(edit) 输出字段添加描述

--- src/form-components/properties-edit/property-edit.tsx
+++ src/form-components/properties-edit/property-edit.tsx
@@ -1,11 +1,11 @@
 import React, { useLayoutEffect, useState } from 'react';
 
-import { IconCrossCircleStroked } from '@douyinfe/semi-icons';
-import { Button, Input } from '@douyinfe/semi-ui';
+import { IconCrossCircleStroked, IconExpand, IconShrink } from '@douyinfe/semi-icons';
+import { Button, IconButton, Input } from '@douyinfe/semi-ui';
 import { DynamicValueInput, TypeSelector } from '@flowgram.ai/form-materials';
 
 import { JsonSchema } from '../../typings';
-import { LeftColumn, Row } from './styles';
+import { ExpandDetail, Label, LeftColumn, Row } from './styles';
 
 export interface PropertyEditProps {
   propertyKey: string;
@@ -19,6 +19,8 @@ export interface PropertyEditProps {
 export const PropertyEdit: React.FC<PropertyEditProps> = (props) => {
   const { value, disabled } = props;
   const [inputKey, updateKey] = useState(props.propertyKey);
+  const [expand, setExpand] = useState(false);
+  
   const updateProperty = (key: keyof JsonSchema, val: any) => {
     value[key] = val;
     props.onChange(value, props.propertyKey);
@@ -31,47 +33,69 @@ export const PropertyEdit: React.FC<PropertyEditProps> = (props) => {
   useLayoutEffect(() => {
     updateKey(props.propertyKey);
   }, [props.propertyKey]);
+
   return (
-    <Row>
-      <LeftColumn>
-        <TypeSelector
-          value={value}
-          disabled={disabled}
-          style={{ position: 'absolute', top: 2, left: 4, zIndex: 1, padding: '0 5px', height: 20 }}
-          onChange={(val) => partialUpdateProperty(val)}
-        />
-        <Input
-          value={inputKey}
-          disabled={disabled}
-          size="small"
-          onChange={(v) => updateKey(v.trim())}
-          onBlur={() => {
-            if (inputKey !== '') {
-              props.onChange(value, props.propertyKey, inputKey);
-            } else {
-              updateKey(props.propertyKey);
-            }
-          }}
-          style={{ paddingLeft: 26 }}
-        />
-      </LeftColumn>
-      {
+    <>
+      <Row>
+        <LeftColumn>
+          <TypeSelector
+            value={value}
+            disabled={disabled}
+            style={{ position: 'absolute', top: 2, left: 4, zIndex: 1, padding: '0 5px', height: 20 }}
+            onChange={(val) => partialUpdateProperty(val)}
+          />
+          <Input
+            value={inputKey}
+            disabled={disabled}
+            size="small"
+            onChange={(v) => updateKey(v.trim())}
+            onBlur={() => {
+              if (inputKey !== '') {
+                props.onChange(value, props.propertyKey, inputKey);
+              } else {
+                updateKey(props.propertyKey);
+              }
+            }}
+            style={{ paddingLeft: 26 }}
+          />
+        </LeftColumn>
         <DynamicValueInput
           value={value.default}
           onChange={(val) => updateProperty('default', val)}
           schema={{...value, extra: { weak: true }}}
-          style={{ flexGrow: 1 , }}
+          style={{ flexGrow: 1 }}
         />
-      }
-      {props.onDelete && !disabled && (
-        <Button
-          style={{ marginLeft: 5, position: 'relative', top: 2 }}
-          size="small"
-          theme="borderless"
-          icon={<IconCrossCircleStroked />}
-          onClick={props.onDelete}
-        />
+        {!disabled && (
+          <IconButton
+            style={{ marginLeft: 5, position: 'relative', top: 2 }}
+            size="small"
+            theme="borderless"
+            icon={expand ? <IconShrink size="small" /> : <IconExpand size="small" />}
+            onClick={() => setExpand(!expand)}
+          />
+        )}
+        {props.onDelete && !disabled && (
+          <Button
+            style={{ marginLeft: 5, position: 'relative', top: 2 }}
+            size="small"
+            theme="borderless"
+            icon={<IconCrossCircleStroked />}
+            onClick={props.onDelete}
+          />
+        )}
+      </Row>
+      {expand && (
+        <ExpandDetail>
+          <Label>describe</Label>
+          <Input
+            size="small"
+            value={value.description || ''}
+            onChange={(description) => updateProperty('description', description)}
+            placeholder="输入字段描述,帮助用户理解该字段的用途"
+            style={{ flex: 1 }}
+          />
+        </ExpandDetail>
       )}
-    </Row>
+    </>
   );
 };
--- src/form-components/properties-edit/styles.tsx
+++ src/form-components/properties-edit/styles.tsx
@@ -17,3 +17,23 @@ export const LeftColumn = styled.div`
   flex: 0 0 120px;
   position: relative;
 `;
+
+export const ExpandDetail = styled.div`
+  margin-left: 0px;
+  margin-bottom: 4px;
+  margin-top: -2px;
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  font-size: 12px;
+`;
+
+export const Label = styled.div`
+  font-size: 12px;
+  color: rgba(0, 0, 0, 0.6);
+  font-weight: 500;
+  white-space: nowrap;
+  min-width: 120px;
+  flex: 0 0 120px;
+  padding-left: 8px;
+`;
